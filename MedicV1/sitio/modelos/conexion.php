b5b0ca99fb8f94312e5110a3182ac7a58eaff95f
christian-lp <142533863+christian-lp@users.noreply.github.com>
2023-08-23 16:51:04 -0300

    Delete MedicV1 directory

--- MedicV1/sitio/modelos/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class Conexion {
-	static public function conectar(){
-		// PDO("nombre del servidor; nombre de la DB","usuario","contaseña");
-		$link = new PDO("mysql:host=".getenv("SQL_SERVER")."; dbname=".getenv("MEDIC_DB"), getenv("MEDIC_DB_USER"), getenv("MEDIC_DB_PASS"));
-		$link->exec("set names utf8");
-		return $link;
-	}
-}
